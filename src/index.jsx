a378f9ae3d18ff027d7a770205bfd18b998e75dc
bbrvi1993@gmail.com <bbrvi1993@gmail.com>
2020-11-22 18:37:20 +0700

    Fix bug

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoApp from './components/TodoApp';
+
+import './index.css';
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root')
+);
